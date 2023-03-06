24c3a2a78c8df33dd3dc6c77772b8d6d79d54762
harshitg1 <harshit31.ugcs20@iiitranchi.ac.in>
2023-03-06 14:18:02 +0530

    updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,35 @@
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 import Cart from "./Cart";
-import { useStateContext } from "../context/stateContext"
+import { useStateContext } from "../context/stateContext";
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities} = useStateContext()
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
-    <div className="navbar-container">
-      <p className="logo">
+    <div
+      className="flex 
+    border
+    justify-between
+    my-[6px]
+    mx-[18px]
+    relative "
+    >
+      <p className="  text-gray-600  text-lg">
         <Link href="/">Store</Link>
       </p>
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
+        <span
+          className="absolute  mt-[-8px]  text-[12px]  text-[#eee]  bg-[#f02d34] w-[18px] h-[18px] font-semibold rounded-[50%]  text-center" >
+          {totalQuantities}
+        </span>
       </button>
-      {showCart && <Cart/>}
+      {showCart && <Cart />}
     </div>
   );
 };
